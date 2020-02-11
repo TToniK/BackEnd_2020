11fbbe6dbbb8f13dcd76136d90f1d42f73ec279c
Toni Kari <TToniK@users.noreply.github.com>
2020-02-11 10:33:26 +0200

    passwordhash started

--- BankTest/BankTest/Services/UserService/IUserservice.cs
+++ BankTest/BankTest/Services/UserService/IUserservice.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BankTest.Models;
 
 namespace BankTest.Services.UserService
 {
     interface IUserservice
     {
+        User Create(User user);
+
+        List<User> Read();
+
+        User read(int id);
+
+        User update(int id, User user);
+
+        User delete(int id);
     }
 }
--- BankTest/BankTest/Services/UserService/Userservice.cs
+++ BankTest/BankTest/Services/UserService/Userservice.cs
@@ -1,11 +1,46 @@
-﻿using System;
+﻿using BankTest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BankTest.Repositories;
+using BankTest.Authentication;
 
 namespace BankTest.Services.UserService
 {
-    public class Userservice
+    public class Userservice : IUserservice
     {
+        private readonly IUserrepository _userrepository;
+
+        public Userservice(IUserrepository userrepository)
+        {
+            _userrepository = userrepository;
+        }
+        public User Create(User user)
+        {
+            user.Salt = PasswordHash.MakeSalt();
+            user.PswHash = PasswordHash.HashPassword(user.PswHash, user.Salt);
+            return _userrepository.Create(user);
+        }
+
+        public User delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User update(int id, User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
